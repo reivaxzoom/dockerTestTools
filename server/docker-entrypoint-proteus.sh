1e561e7904bab1cd2f8f589b62090a494f47d795
reivaxzoom <sxavier@inf.elte.hu>
2017-06-22 15:44:08 -0500

    Update docker-entrypoint-proteus.sh

--- server/docker-entrypoint-proteus.sh
+++ server/docker-entrypoint-proteus.sh
@@ -2,11 +2,11 @@
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 # 
 # Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
-if [[ -z $ADMIN_PASSWORD ]]; then
+#if [[ -z $ADMIN_PASSWORD ]]; then
 	ADMIN_PASSWORD=$(date| md5sum | fold -w 8 | head -n 1)
 	echo "##########GENERATED ADMIN PASSWORD: $ADMIN_PASSWORD  ##########"
-fi
-echo "setting resources proteus"
+#fi
+#echo "setting resources proteus"
 asadmin create-jdbc-connection-pool --datasourceclassname com.microsoft.sqlserver.jdbc.SQLServerDataSource --restype jav$
 asadmin create-jdbc-resource --connectionpoolid proteus jdbc/proteus
 asadmin restart-domain domain1 && \
@@ -15,6 +15,6 @@ asadmin create-jms-resource --restype javax.jms.ConnectionFactory --description 
 asadmin create-jms-resource --restype javax.jms.Topic --property Name=proteusCargaMasivaTramiteQueue --description "Cola$
 create-jms-resource --restype javax.jms.Topic --property Name=proteusCargaMasivaTramiteQueue --description "Cola geeraci$
 create-jms-resource --restype javax.jms.Topic --property Name=proteusCargaMasivaTramiteQueue --description "Cola generac$
-create-javamail-resource --mailhost 172.20.24.44 --mailuser sigen --description mailResourceProteus --fromaddress sigen@quito.gob.ec  &\\ 
+create-javamail-resource --mailhost 172.20.24.44 --mailuser sigen --description mailResourceProteus --fromaddress sigen@$
 
 exec "$@"
