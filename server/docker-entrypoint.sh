f7fadd1f64ea62d5928cdd9b0e04c47ddbd881aa
reivaxzoom <sxavier@inf.elte.hu>
2017-06-22 16:31:02 -0500

    Update docker-entrypoint.sh

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,17 +1,18 @@
-#!/bin/bash
+!/bin/bash
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 # 
 # Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
-if [[ -z $ADMIN_PASSWORD ]]; then
-	ADMIN_PASSWORD=$(date| md5sum | fold -w 8 | head -n 1)
-	echo "##########GENERATED ADMIN PASSWORD: $ADMIN_PASSWORD  ##########"
-fi
-echo "AS_ADMIN_PASSWORD=" > /tmp/glassfishpwd
-echo "AS_ADMIN_NEWPASSWORD=${ADMIN_PASSWORD}" >> /tmp/glassfishpwd
-asadmin --user=admin --passwordfile=/tmp/glassfishpwd change-admin-password --domain_name domain1
-asadmin start-domain
-echo "AS_ADMIN_PASSWORD=${ADMIN_PASSWORD}" > /tmp/glassfishpwd
-asadmin --user=admin --passwordfile=/tmp/glassfishpwd enable-secure-admin
-asadmin --user=admin stop-domain
-rm /tmp/glassfishpwd
+
+
+asadmin create-jdbc-connection-pool --datasourceclassname com.microsoft.sqlserver.jdbc.SQLServerDataSource --restype javax.sql.DataSource --property user=migracion:password='migra$2k12':DatabaseName=proteus:ServerName=172.22.16.73:port=1433 proteus 
+asadmin create-jdbc-resource --connectionpoolid proteus jdbc/proteus 
+asadmin ping-connection-pool proteus  
+asadmin create-jms-resource --restype javax.jms.ConnectionFactory --description "Cola de mesajeria para proteus"  jms/proteusConnectionFactory                                              
+asadmin create-jms-resource --restype javax.jms.Topic --property Name=proteusCargaMasivaTramiteQueue --description "Cola carga masiva tramite" jms/proteusCargaMasivaTramiteQueue           
+asadmin create-jms-resource --restype javax.jms.Topic --property Name=proteusCargaMasivaTramiteQueue --description "Cola geeracion de carga masiva claves" jms/proteusGenMasivaClavesQueue  
+asadmin create-jms-resource --restype javax.jms.Topic --property Name=proteusCargaMasivaTramiteQueue --description "Cola generacion nomina" jms/proteusGeneracionNominaQueue                
+asadmin create-javamail-resource --mailhost 172.20.24.44 --mailuser sigen --description mailResourceProteus --fromaddress sigen@quito.gob.ec mail/proteus                                   
+
+
+
 exec "$@"
