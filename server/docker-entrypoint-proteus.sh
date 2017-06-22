874bed08fc377b5f271044c8fad579cb727d0a4c
reivaxzoom <sxavier@inf.elte.hu>
2017-06-22 15:44:46 -0500

    test

--- server/docker-entrypoint-proteus.sh
+++ server/docker-entrypoint-proteus.sh
@@ -7,14 +7,4 @@
 	echo "##########GENERATED ADMIN PASSWORD: $ADMIN_PASSWORD  ##########"
 #fi
 #echo "setting resources proteus"
-asadmin create-jdbc-connection-pool --datasourceclassname com.microsoft.sqlserver.jdbc.SQLServerDataSource --restype jav$
-asadmin create-jdbc-resource --connectionpoolid proteus jdbc/proteus
-asadmin restart-domain domain1 && \
-asadmin ping-connection-pool proteus && \
-asadmin create-jms-resource --restype javax.jms.ConnectionFactory --description "Cola de mesajeria para proteus"  jms/pr$
-asadmin create-jms-resource --restype javax.jms.Topic --property Name=proteusCargaMasivaTramiteQueue --description "Cola$
-create-jms-resource --restype javax.jms.Topic --property Name=proteusCargaMasivaTramiteQueue --description "Cola geeraci$
-create-jms-resource --restype javax.jms.Topic --property Name=proteusCargaMasivaTramiteQueue --description "Cola generac$
-create-javamail-resource --mailhost 172.20.24.44 --mailuser sigen --description mailResourceProteus --fromaddress sigen@$
-
 exec "$@"
